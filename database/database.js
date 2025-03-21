170c21b044be278068f222535524e27d8e315890
Mariana Hoyos <hoyosmariana62@gmail.com>
2025-03-20 19:54:30 -0500

    se agregan comentarios sobre la función de cada parte del codigo

--- database/database.js
+++ database/database.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
+require('dotenv').config(); //carga las variables de  entorno desde .env
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+const db = mysql.createConnection({  //crea una conexión a la base de datos
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
-db.connect(err => {
+db.connect(err => { //intenta conectar y muestra mensaje en la terminal
     if (err){
         console.error('Error al conectar a la base de datos:', err);
         return;
@@ -17,4 +17,4 @@ db.connect(err => {
     
 });
 
-module.exports = db;
+module.exports = db; // exporta la conexión para usarla en otros archivos
