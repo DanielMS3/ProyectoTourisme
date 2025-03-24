db609169c68f6235c5dd4e68e03fd0507f1f7014
Miguel Angel <cardonam2006@gmail.com>
2025-03-24 14:47:57 -0500

    Se mofifica el codigo de los archivos ContentManager y index

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import ContentManager from "./ContentManager";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <ContentManager />
+    <div style={{ maxWidth: "800px", margin: "0 auto", padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ textAlign: "center" }}>Gestión de Contenidos</h1>
+      <ContentManager />
+    </div>
   </React.StrictMode>
 );
