7ffe697cbc2281dcc70f9f845582433d22d5173e
Daniel Moreno <danielricardomorenortiz@gmail.com>
2025-03-29 10:28:44 -0500

    Adición de código para el CRUD

--- CRUD/client/src/App.js
+++ CRUD/client/src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="datos">
+
+        <label>Nombre: <input type="text"/></label>
+        <label>Edad: <input type="text"/></label>
+        <label>País: <input type="text"/></label>
+        <label>Cargo: <input type="text"/></label>
+        <label>Años: <input type="text"/></label>
+        <button>Registrar</button>
+
+
+      </div>
+
     </div>
   );
 }
