2bffd25816f7df99c75ea5e8b03d08e61bc80e26
Mariana Hoyos <hoyosmariana62@gmail.com>
2025-03-30 14:00:45 -0500

    se elimina archivo porque es redundante y genera conflicto

--- index.js
+++ index.js
@@ -1,56 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken");
-const db = require("./server/database/database");
-
-dotenv.config();
+const express = require('express');
+const path = require('path');
 const app = express();
 
+// Configuración
+app.set('port', process.env.PORT || 3000);
+
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "client", "public")));
-
+// Conexión a la base de datos
+require('./server/database/database.js');
 
 // Rutas
-const registroRoutes = require("./server/routes/registro_conexion");
-app.use("/api", registroRoutes);
+const routes = require('./server/routes/index');
+app.use('/', routes);
 
-const loginRoute = require("./server/routes/login");
-app.use("/login", loginRoute);
-
-const recuperarContrasenaRoutes = require('./server/routes/recuperar_contrasena');
-app.use('/api', recuperarContrasenaRoutes);
-
-
-
-
-// Ruta protegida /perfil
-app.get("/perfil", (req, res) => {
-    const authHeader = req.headers["authorization"];
-
-    if (!authHeader) {
-        return res.status(401).json({ error: "No autorizado: No se proporcionó un token" });
-    }
-
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, process.env.JWT_SECRET || "clave_secreta", (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ error: "Token inválido o expirado" });
-        }
-
-        res.json({ user: { email: decoded.email } });
-    });
-});
-
-// Iniciar servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// Iniciar el servidor
+app.listen(app.get('port'), () => {
+    console.log(`Servidor corriendo en http://localhost:${app.get('port')}`);
 });
