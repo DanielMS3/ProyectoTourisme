8de39e58a1fdc48ee543be1b484e52cd6589a39c
Mariana Hoyos <hoyosmariana62@gmail.com>
2025-03-30 19:41:55 -0500

    modifico

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const app = require("./server/server");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
