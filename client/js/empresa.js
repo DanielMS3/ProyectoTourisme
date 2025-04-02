026374e92d0876a4e560bffa9c64fd149b5659eb
Mariana Hoyos <hoyosmariana62@gmail.com>
2025-04-02 10:31:31 -0500

    export

---
+++ client/js/empresa.js
@@ -0,0 +1,11 @@
+export default class Empresa {
+    constructor(nombre, direccion, telefono, email, web, logo, id) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+        this.web = web;
+        this.logo = logo;
+        this.id = id;
+    }
+}
